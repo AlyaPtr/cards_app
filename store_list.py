16d771dc93ebac5141cb4ebd27ff4786b7c49e04
BaDWerewolf <bad.werewolf@outlook.com>
2022-12-01 21:04:17 +0700

    Add square generating

--- store_list.py
+++ store_list.py
@@ -1,10 +1,14 @@
 from OSMPythonTools.nominatim import Nominatim
 from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
+#https://overpass-turbo.eu/# для проверки квадрата
+#https://habr.com/ru/company/vk/blog/591879/ язык разметки
+#http://bboxfinder.com/#54.840143,83.099232,54.842943,83.103631 генератор квадрата
 
-nominatim = Nominatim()
-areaID = nominatim.query([83.100483,54.840505,83.103755,54.842266]).areaId()
 
-query = overpassQueryBuilder(bbox=[54.840143,83.099232,54.842943,83.103631],elementType='node', selector="shop", out='body')
+# nominatim = Nominatim()
+# areaID = nominatim.query([83.100483,54.840505,83.103755,54.842266]).areaId()
+
+query = overpassQueryBuilder(bbox=[54.841470,83.101153,54.842415,83.102790],elementType='node', selector="shop", out='body')
 print(query)
 overpass = Overpass()
 shops = overpass.query(query)
