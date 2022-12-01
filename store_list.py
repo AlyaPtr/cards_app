f80b0bc19bcf520b25351e4bd0b3c9b991a1ab3e
BaDWerewolf <bad.werewolf@outlook.com>
2022-12-01 21:05:44 +0700

    Add square generating

--- store_list.py
+++ store_list.py
@@ -4,7 +4,29 @@ from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
 #https://habr.com/ru/company/vk/blog/591879/ язык разметки
 #http://bboxfinder.com/#54.840143,83.099232,54.842943,83.103631 генератор квадрата
 
+class RangeBox:
+    def __init__(self, current_position):
+        """current_position is a tuple of degree coordinates (a, b) where
 
+            a - latitude
+            b - longitude
+        """
+
+        __RANGE = 0.00117
+
+
+        self.position = current_position
+    
+    def bbox(self) -> tuple:
+        """ Returns a bound box. """
+        box = [ self.position[0] - self.RANGE, 
+                self.position[1] + self.RANGE, 
+                self.position[0] + self.RANGE, 
+                self.position[0] - self.RANGE]
+        return box
+        
+
+#0.00117
 # nominatim = Nominatim()
 # areaID = nominatim.query([83.100483,54.840505,83.103755,54.842266]).areaId()
 
