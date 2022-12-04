5a4249ad8d06dec29347c567c8832051127bd005
SergKurchev <119513151+SergKurchev@users.noreply.github.com>
2022-12-05 03:02:54 +0700

    QRCODE,EAN13,EAN14,EAN8,CODE128,CODE39,UPCA,I25

--- barcode/bar_code_scaner_and_generate_app_kv.py
+++ barcode/bar_code_scaner_and_generate_app_kv.py
@@ -4,30 +4,36 @@
 Картинка хранится под именем "barcode.png"
 Есть возможность изменения формата сохранения штрих-кода
 
-Сейчас работает только для QRCODE, EAN13, CODE128, I25
+Сейчас работает только для QR-CODE, EAN 13, EAN 14, EAN 8, CODE 128, CODE 39, UPC-A, I25
 """
 
 # if you on Windows you need to install vcredist_x64.exe from https://aka.ms/highdpimfc2013x64enu
 # pip install zbarcam
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.core.window import Window
 
 from kivy_garden.zbarcam import ZBarCam
-from pyzbar.pyzbar import ZBarSymbol
 from kivy.uix.image import Image
 from kivy.clock import Clock
 
 # pip install python-barcode[images]
 import barcode
 from barcode.ean import EAN13
+from barcode.ean import EAN14
+from barcode.ean import EAN8
+from barcode.ean import JAN
+from barcode.codabar import CODABAR
 from barcode.codex import Code128
+from barcode.codex import Code39
+from barcode.upc import UPCA
 from barcode.itf import ITF #I25
 
 # pip install qrcode
 import qrcode
+
+from barcode.writer import ImageWriter
 
 class CardScaner(BoxLayout):
     def __init__(self, **kwargs):
@@ -36,18 +42,14 @@ class CardScaner(BoxLayout):
         self.label_with_instruction = Label(text="Наведите камеру на штрих-код карты пожалуйста")
         self.add_widget(self.label_with_instruction)
         self.zbarcam= ZBarCam()
-        #code_types=[ZBarSymbol.EAN13]
-        #self.add_widget(self.zbarcam)  
-        #self.zbarcam.size = (Window.size[0], Window.size[1])  
-
         self.add_widget(self.zbarcam)
 
         self.clock = Clock.schedule_interval(self.read_qr_text, 1)
 
     def read_qr_text(self, *args):
+        self.zbarcam.export_to_png("last_shoot.png")
         """Check if zbarcam.symbols is filled and stop scanning in such case"""
         if(len(self.zbarcam.symbols) > 0): # when something is detected
-            #print(', '.join([str(symbol) for symbol in self.zbarcam.symbols]))
             standart_of_code = self.zbarcam.symbols[0].type # type of standart of code (one of card_information or qr-code)
             texted_code = self.zbarcam.symbols[0].data.decode('UTF-8') # information from card
             if standart_of_code == 'QRCODE':
@@ -56,25 +58,44 @@ class CardScaner(BoxLayout):
             elif standart_of_code == 'EAN13':
                 img = EAN13(texted_code, writer=ImageWriter("PNG"))
                 img.save("card_information")
+            elif standart_of_code == 'EAN14':
+                img = EAN14(texted_code, writer=ImageWriter("PNG"))
+                img.save("card_information")
+            elif standart_of_code == 'EAN8':
+                img = EAN8(texted_code, writer=ImageWriter("PNG"))
+                img.save("card_information")
+            elif standart_of_code == 'JAN':
+                img = JAN(texted_code, writer=ImageWriter("PNG"))
+                img.save("card_information")
+            elif standart_of_code == 'CODABAR':
+                img = CODABAR(texted_code, writer=ImageWriter("PNG"))
+                img.save("card_information")
             elif standart_of_code == 'CODE128':
                 img = Code128(texted_code, writer=ImageWriter("PNG"))
+                img.save("card_information")
+            elif standart_of_code == 'CODE39':
+                img = Code39(texted_code, writer=ImageWriter("PNG"))
+                img.save("card_information")
+            elif standart_of_code == 'UPCA':
+                img = UPCA(texted_code, writer=ImageWriter("PNG"))
                 img.save("card_information")
             elif standart_of_code == 'I25':
                 img = ITF(texted_code, writer=ImageWriter("PNG"))
                 img.save("card_information")
             else:
                 return
+
             # image with generated code saved as card_information.png
+            self.imaged_barcode = Image(source="card_information.png")    
 
             Clock.unschedule(self.clock) # stop repeating
             self.zbarcam.stop() # stop zbarcam
             self.zbarcam.ids['xcamera']._camera._device.release() # release camera
-
-            self.imaged_barcode = Image(source="card_information.png")   
-                
+               
             self.remove_widget(self.zbarcam)
             self.remove_widget(self.label_with_instruction)
             self.add_widget(self.imaged_barcode)
+
 
 
 class CardScanerApp(App):
